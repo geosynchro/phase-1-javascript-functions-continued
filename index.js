72ab25bc8d30dcbacdbc0413b4059282b8ab0c5f
Nicholas Fagner <nicholasfagner@gmail.com>
2021-10-12 20:03:10 -0600

    commit tree 905e9afe23b32adf6d62d96d3d432d44881e5cb2

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+function saturdayFun(input = "roller-skate"){
+    return `This Saturday, I want to ${input}!`
+}
+
+function mondayWork(action = "go to the office"){
+    return `This Monday, I will ${action}.`
+}
+
+
+function wrapAdjective(emphatic = "*"){
+    const you = "You are"
+    return function(text){
+        return `${you} ${emphatic}${text}${emphatic}!`;
+    };
+}
+
+console.log(wrapAdjective()("a hard worker"))
